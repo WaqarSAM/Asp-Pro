c9650b455ec7dd7133af94ecce29c2c491ea8f57
waqar <waqar@DESKTOP-80TA7ML>
2018-10-10 22:13:26 +0500

    Add project files.

---
+++ Asp Pro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp_Pro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
